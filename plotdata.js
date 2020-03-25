693741406e9f0cd04fa56633038bb16a52c33e89
Kevin <kevinski303@protonmail.com>
2020-03-25 15:03:22 +0100

    wip

--- plotdata.js
+++ plotdata.js
@@ -1,55 +1,37 @@
+var csvdata1 = "https://raw.githubusercontent.com/daenuprobst/covid19-cases-switzerland/master/covid19_cases_switzerland.csv";
+var csvdata2 = "https://raw.githubusercontent.com/daenuprobst/covid19-cases-switzerland/master/covid19_fatalities_switzerland.csv";
+
 function makeplot() {
-  Plotly.d3.csv("https://raw.githubusercontent.com/daenuprobst/covid19-cases-switzerland/master/covid19_cases_switzerland.csv", 
-  function(data) {
-    processData(data)
+  Plotly.d3.csv(csvdata1, function(data1) {
+    processData(data1);
+    console.log(data1);
+  });
+  Plotly.d3.csv(csvdata2, function(data2) {
   });
 };
 
 function processData(allRows) {
+  
   //console.log(allRows);
-
-  var x = [], y = [], standard_deviation = [];
+  var xDate = [], yCH = [], standard_deviation = [];
 
   for (var i=0; i<allRows.length; i++) {
     row = allRows[i];
-    x.push( row['Date'] );
-    y.push( row['CH'] );
+    xDate.push( row['Date'] );
+    yCH.push( row['CH'] );
   }
-
-  //console.log( 'X',x, 'Y',y, 'SD',standard_deviation );
-  makePlotly( x, y, standard_deviation );
+  //console.log( 'X',xDate, 'Y',yCH, 'Y2',yBE, 'SD',standard_deviation );
+  makePlotly( xDate, yCH, standard_deviation);
 };
 
-function makePlotly( x, y, standard_deviation ){
-  var plotdiv = document.getElementById("plot");
-  //console.log(plotdiv);
-  var trace = [{
-    x: x,
-    y: y,
-    line: {
-      shape: 'spline'
-    }
-  }];
-
-  var conf = {
-    responsive: true,
-    displayModeBar: false
-  };
-
-  var layout = {
-    title: 'COVID-19 confirmed cases in Switzerland',
-    font: { size: 18 },
-    height: 500,
-    width: 800,
-    autosize: true,
-    xaxis: {
-      tickangle: 45,
-    },
-    yaxis: {
-      title: ''
-    }
-  };
-
-  Plotly.newPlot(plotdiv, trace, layout, conf);
+function makePlotly( xDate, yCH){
+  var plotDiv = document.getElementById("plot");
+  var trace1 = {x:xDate, y:yCH, line: { shape: 'spline' }}
+  var trace2 = {x:xDate}
+  var traces = [ trace1, trace2 ];
+  var conf = { responsive: true, displayModeBar: false };
+  var layout = { title: '', font: { size: 18 }, height: 500, width: 800, autosize: true, xaxis: { tickangle: 45, } };
+  
+  Plotly.newPlot(plotDiv, traces, layout, conf);
 };
 makeplot();
