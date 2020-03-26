215fe9b1cb1dcf08c4d93204ad0a83d2f695714d
Kevin <kevinski303@protonmail.com>
2020-03-26 23:58:25 +0100

    added total deaths

--- scripts/plotData.js
+++ scripts/plotData.js
@@ -1,47 +1,17 @@
 var csv1 = "https://raw.githubusercontent.com/daenuprobst/covid19-cases-switzerland/master/covid19_cases_switzerland.csv"; 
 var csv2 = "https://raw.githubusercontent.com/daenuprobst/covid19-cases-switzerland/master/covid19_fatalities_switzerland.csv";
 
-function makePlotTotalPositive(csv) {
-  Plotly.d3.csv(csv, function(csvdata) {
-    processDataTotalPositive(csvdata);
+/**
+ * Load CSV and run DataProcess
+ * 
+ */
+
+function makeAllPlots() {
+  Plotly.d3.csv(csv1, function(csvdata1) {
+    processDataTotalPositive(csvdata1);
+    processDataNewCaseesPerDay(csvdata1);
+  });
+  Plotly.d3.csv(csv2, function(csvdata2) {
+    processDataTotalDeath(csvdata2);
   });
 };
-
-function makePlotNewCasesPerDay(csv) {
-  Plotly.d3.csv(csv, function(csvdata) {
-    processDataNewCaseesPerDay(csvdata);
-  });
-};
-
-function processDataTotalPositive(allRows) {
-  var date = [], CH = [], plotTitle = 'Total positive tested cases', divId = 'totalPositivePlot';
-  for (var i=0; i < allRows.length; i++) {
-    row = allRows[i];
-    date.push( row['Date'] );
-    CH.push( row['CH'] );
-  };
-  makeLinearPlotXY(date, CH, plotTitle, divId)
-};
-
-function processDataNewCaseesPerDay(allRows) {
-  var dateBar = [], chBar = [], plotTitleBar = 'New positive tested cases per day', divIdBar = 'newCasesPerDay', previousCH = 0;
-  for (var i=0; i < allRows.length; i++) {
-    //console.log('previousRowCH' + previousCH);
-    row = allRows[i];
-    dateBar.push( row['Date'] );
-    rowCH = row['CH']
-    //console.log( 'rowCH' + rowCH);
-    rowCHdiff = rowCH - previousCH;
-    chBar.push(rowCHdiff);
-    //console.log(rowCHdiff);
-    previousCH = rowCH;
-    //chHistogram.push( rowCH );
-    //console.log(dateHistogram, chHistogram);
-
-  };
-
-  makeBarChart( dateBar, chBar, plotTitleBar, divIdBar)
-};
-
-makePlotTotalPositive(csv1);
-makePlotNewCasesPerDay(csv1);
--- scripts/plotTemplates.js
+++ scripts/plotTemplates.js
@@ -1,14 +1,14 @@
 function makeLinearPlotXY(xgiven , ygiven, headTitle, divElement){
   var plotDiv = document.getElementById(divElement);
-  var trace = [{x:xgiven, y:ygiven, line: { shape: 'spline' }}]
+  var trace = [{x:xgiven, y:ygiven, line: { shape: 'spline'}, type: 'scatter', fill: 'tonexty'}]
   var conf = { responsive: true, displayModeBar: false };
-  var layout = { title: headTitle, font: { size: 14 }, height: 500, width: 800, autosize: true, xaxis: { tickangle: 45, }, yaxis: { }, paper_bgcolor: '#F5F5F5', plot_bgcolor: '#F5F5F5' };
+  var layout = { title: headTitle, font: { size: 14 }, height: 500, width: 800, autosize: true, xaxis: { tickangle: 45 }, yaxis: { }, paper_bgcolor: '#F5F5F5', plot_bgcolor: '#F5F5F5' };
   Plotly.newPlot(plotDiv, trace, layout, conf);
 }
 
 function makeBarChart(xgiven , ygiven, headTitleHistogram, divElementHistogram){
   var plotDiv = document.getElementById(divElementHistogram);
-  var trace = [{x:xgiven, y:ygiven, type: 'bar'}]
+  var trace = [{x:xgiven, y:ygiven, type: 'bar', marker: { opacity: 0.6, line: { color: 'rgb(8,48,107)', width: 0.5 } } }]
   var conf = { responsive: true, displayModeBar: false };
   var layout = { title: headTitleHistogram, font: { size: 14 }, height: 500, width: 800, autosize: true, xaxis: { tickangle: 45, }, yaxis: { }, bargap: 0.01, paper_bgcolor: '#F5F5F5', plot_bgcolor: '#F5F5F5' };
   Plotly.newPlot(plotDiv, trace, layout, conf);
